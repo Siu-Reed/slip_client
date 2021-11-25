14a6ea4379ba20832ed1dfb68848f61b26250dcc
Siu-Reed <tldn960823@gmail.com>
2021-11-25 16:41:41 +0900

    [add]

--- src/network/socket.js
+++ src/network/socket.js
@@ -3,7 +3,7 @@ import socket from 'socket.io-client';
 export default class Socket {
     constructor(baseURL, getAccessToken) {
         this.io = socket(baseURL, {
-            auth: (cb) => cb({ token: getAccessToken() }),
+            auth: (cb) => cb({ token: getAccessToken }),
             // 중요 한 번 더 보셈
             // auth 아닌 query 사용 시 브라우저에서 토큰이 보일 수 있음..
             // handshake 안에 있는 auth 사용. 보안을 위해
